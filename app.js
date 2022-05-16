af5f77bd63b6ed2c232d361fefdeec1e561e3c87
Avetian <Ani.Avetian1@T-Mobile.com>
2022-05-16 14:54:57 -0700

    fixed items to match description

--- app.js
+++ app.js
@@ -8,10 +8,6 @@ app.use(express.urlencoded({ extended: true }));
 let LIBRARY = [];
 let LENGTH = 0;
 
-app.get("/", (req, res) => {
-  res.status(200).send("App is Working!");
-});
-
 app.get("/health", (req, res) => {
   res.status(200).send("Don't panic.");
 });
@@ -20,35 +16,38 @@ app.get("/health", (req, res) => {
 app.post("/api/books", function(req, res) {
   const author = req.body.author;
   const title = req.body.title;
-  const year = req.body.year;
+  const yearPublished = req.body.yearPublished;
 
-  if (author && title && year) {
+  if (author && title && yearPublished) {
     let book = {
       "id": (LENGTH + 1),
       "author": author,
       "title": title,
-      "year": year
+      "yearPublished": yearPublished
     };
     LENGTH = LENGTH + 1;
     LIBRARY.push(book);
-    res.status(201).send({
-      "author": author,
-      "title": title,
-      "year": year});
+    res.status(201).send(book);
   } else {
     res.status(400).send("Parameters are not present. Please try again!")
   } 
 });
 
 app.get("/api/books", function(req, res) {
-  let sortedBooks = LIBRARY;
-  sortedBooks.sort((book1, book2) => (book1.author > book2.author) ? 1 : -1)
-  res.status(200).send(sortedBooks);
+  if (LIBRARY.length > 0) {
+    let sortedBooks = LIBRARY;
+    sortedBooks.sort((book1, book2) => (book1.title > book2.title) ? 1 : -1);
+    res.status(200).send(
+      {"books": sortedBooks});
+  }
 });
 
 app.delete("/api/books", function(req, res) {
-  LIBRARY = [];
-  res.status(204).send();
+  if (LIBRARY.length > 0) {
+    LIBRARY = [];
+    LENGTH = 0;
+    res.status(204);
+  }
 });
 
 
