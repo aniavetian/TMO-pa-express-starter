a12902f51cd581b8f70bad2af0b25420b2e0a066
Avetian <Ani.Avetian1@T-Mobile.com>
2022-05-16 16:36:49 -0700

    added code back and removed public dependency

--- app.js
+++ app.js
@@ -13,8 +13,42 @@ app.get("/health", (req, res) => {
 });
 
 
+app.post("/api/books", function(req, res) {
+  const author = req.body.author;
+  const title = req.body.title;
+  const yearPublished = req.body.yearPublished;
 
+  if (author && title && yearPublished) {
+    let book = {
+      "id": (LENGTH + 1),
+      "author": author,
+      "title": title,
+      "yearPublished": yearPublished
+    };
+    LENGTH = LENGTH + 1;
+    LIBRARY.push(book);
+    res.status(201).send(book);
+  } else {
+    res.status(400).send("Parameters are not present. Please try again!")
+  } 
+});
+
+app.get("/api/books", function(req, res) {
+  if (LIBRARY.length > 0) {
+    let sortedBooks = LIBRARY;
+    sortedBooks.sort((book1, book2) => (book1.title > book2.title) ? 1 : -1);
+    res.status(200).send(
+      {"books": sortedBooks});
+  }
+});
+
+app.delete("/api/books", function(req, res) {
+  if (LIBRARY.length > 0) {
+    LIBRARY = [];
+    LENGTH = 0;
+    res.status(204);
+  }
+});
 
 
 module.exports = app;
-app.use(express.static("public"));
